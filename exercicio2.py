2a638b2f79cdf5df29d27afaa40ea81d8a6dddd6
letyresina <leticiaresina@gmail.com>
2023-09-01 11:48:41 -0300

    subindo exercício 2 (não finalizado, terminar em casa)

---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+'''
+    Exercício 2:
+    Escreva um programa que recebe uma frase como entrada e retorna um dicionário onde as
+    chaves são as palavras únicas na frase e os valores são o número de vezes que cada
+    palavra aparece.
+'''
+
