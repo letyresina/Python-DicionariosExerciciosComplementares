37a716601e8adc95452c6d690f8d161df6f1534b
letyresina <leticiaresina@gmail.com>
2023-09-05 09:26:52 -0300

    Subindo exercício 2

--- exercicio2.py
+++ exercicio2.py
@@ -5,3 +5,16 @@
     palavra aparece.
 '''
 
+dicionario = {} 
+
+def transformaFrase(frase):
+    palavra = frase.split()
+    for i in palavra:
+        if i not in dicionario:
+            dicionario[i] = 1
+        else:
+            dicionario[i] += 1
+    return dicionario
+
+frase = input("Informe uma frase: ")
+print(transformaFrase(frase))
